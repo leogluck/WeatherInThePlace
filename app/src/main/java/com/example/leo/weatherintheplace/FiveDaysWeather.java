1d1a750fadc4c55d62638268dd5161a4fa80b8dd
leogluck <leogluck@gmail.com>
2017-07-24 19:09:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/leo/weatherintheplace/FiveDaysWeather.java
@@ -0,0 +1,13 @@
+package com.example.leo.weatherintheplace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FiveDaysWeather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_five_days_weather);
+    }
+}
---
+++ app/src/main/java/com/example/leo/weatherintheplace/FragmentWeather.java
@@ -0,0 +1,38 @@
+package com.example.leo.weatherintheplace;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentWeather extends Fragment {
+
+    TextView cityField;
+    TextView currentTemperatureField;
+    TextView weatherIcon;
+
+    public FragmentWeather() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_fragment_weather, container, false);
+        cityField = (TextView)rootView.findViewById(R.id.city_field);
+        currentTemperatureField = (TextView)rootView.findViewById(R.id.current_temperature_field);
+        weatherIcon = (TextView)rootView.findViewById(R.id.weather_icon);
+
+        return rootView;
+    }
+
+}
